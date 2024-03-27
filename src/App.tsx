6008a512692637f86c7971d021cb63361335ac94
Youngobz <moerahman1234@gmail.com>
2024-03-27 21:27:07 +0530

    fix: build error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Profiler, useEffect   } from 'react';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css'
 import AutoComplete from './auto-complete'
@@ -11,25 +11,6 @@ function App() {
       console.log(e);
     })
   }, []);
-  const callback = (
-    id, // the "id" prop of the Profiler tree that has just committed
-    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-    actualDuration, // time spent rendering the committed update
-    baseDuration, // estimated time to render the entire subtree without memoization
-    startTime, // when React began rendering this update
-    commitTime, // when React committed this update
-    interactions
-  ) => {
-    console.log({
-      id,
-      phase,
-      actualDuration,
-      baseDuration,
-      startTime,
-      commitTime,
-      interactions,
-    })
-  }
 
   const methodDoesNotExist = () => {
     console.error("Method not exist called")
@@ -38,12 +19,12 @@ function App() {
   return (
     <>
       <AutoComplete />
-      <Profiler  id="AppComponent" onRender={callback}>
+      {/* <Profiler  id="AppComponent" onRender={callback}> */}
         <QueryClientProvider client={queryClient}>
           <Table />
         </QueryClientProvider>
-        <button onClick={() => methodDoesNotExist()}>Break the world</button>;
-      </Profiler> 
+        <button onClick={() => methodDoesNotExist()}>Break the world</button>
+      {/* </Profiler>  */}
     </>
   )
 }
