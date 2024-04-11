2064ca68c98ee74abf51b730f74244c72393098f
Youngobz <moerahman1234@gmail.com>
2024-04-11 21:30:40 +0530

    sentry lazy loaded

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css'
 import AutoComplete from './auto-complete'
 import Table from './table';
+import * as Sentry from "@sentry/react";
 
 function App() {
   const queryClient = new QueryClient();
   useEffect(() => {
     window.addEventListener('online', function (e) {
       console.log(e);
-    })
+    });
+
+    window.onerror(() => {
+      Sentry.init({
+        dsn: "https://b50aabfa86a104b4de78847c2f43771a@o4506983632732160.ingest.us.sentry.io/4506983679524864",
+      })
+    });
   }, []);
 
   const methodDoesNotExist = () => {
